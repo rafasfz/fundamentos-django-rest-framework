3bc51c20cd46df9573eddee32378c7fb6c9c76a6
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2021-12-27 18:49:09 -0300

    Criado metodo get e post para articles

--- api_basic/models.py
+++ api_basic/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 
 
 class Article(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
