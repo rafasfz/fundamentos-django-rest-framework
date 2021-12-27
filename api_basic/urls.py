f400f46a1d300156446fa04e9cbbeac4088eb26c
Rafael Silva Freire <rafaelsilvafreire2@gmail.com>
2021-12-27 19:44:02 -0300

    Craido detalhes, edição e delete de articles

--- api_basic/urls.py
+++ api_basic/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, article_detail
 
 urlpatterns = [
     path('', article_list),
+    path('<str:pk>/', article_detail)
 ]
